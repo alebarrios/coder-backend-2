0c24beacf480d11f579278b9a27f30de94bf444d
Alejandro <al.barrios88@gmail.com>
2025-02-27 23:45:21 -0300

    change users.view.router y add controller

--- src/routes/users.view.router.js
+++ src/routes/users.view.router.js
@@ -1,69 +1,24 @@
 import { Router } from "express";
 import passport from "passport";
+import usersViewController from "../controllers/users.view.controller.js";
 
 const usersViewRouter = Router();
 
-usersViewRouter.get("/login", async (req, res) => {
-    try {
-        if (req.isUnauthenticated()){
-            res.status(200)
-            .render("login", {
-                layout : 'index',
-                style: 'index.css',
-                js: 'login.js',
-                title: "Login",
-                });
-        } else {
-            res.redirect('/products');
-        }
-    } catch (error) {
-        res.status(error.code || 500).send(`<h1>Error</h1><h3>${error.message}</h3>`);
-    }
-});
+usersViewRouter.get("/login", usersViewController.loginUser);
 
-usersViewRouter.get("/register", async (req, res) => {
-    try {
-        if (req.isUnauthenticated()){
-        res.status(200)
-        .render("register", {
-            layout : 'index',
-            style: 'index.css',
-            js: 'register.js',
-            title: "Register",
-            });
-        } else {
-            res.redirect('/products');
-        }
-    } catch (error) {
-        res.status(error.code || 500).send(`<h1>Error</h1><h3>${error.message}</h3>`);
-    }
-});
+usersViewRouter.get("/register", usersViewController.registerUser);
 
 usersViewRouter.post("/register",
     passport.authenticate("register", { failureRedirect: "fail-register" }),
-    async (req, res) => {
-        console.log("Usuario registrado: ", req.session.passport.user);
-        res.redirect("/login");
-    }
-);
+    usersViewController.passportRegistered);
 
-usersViewRouter.get("/fail-register", (req, res) => {
-    console.log("Error o Usuario ya existe. Redireccionando a /register");
-    res.redirect("/register");
-});
+usersViewRouter.get("/fail-register", usersViewController.failRegister);
 
 usersViewRouter.post("/login",
     passport.authenticate("login", { failureRedirect: "fail-login" }),
-    async (req, res) => {
-        console.log("Usuario logeado: ", req.session.passport.user);
-        res.redirect("/products");
-    }
-);
+    usersViewController.passportLogged);
 
-usersViewRouter.get("/fail-login", (req, res) => {
-    console.log("Redireccionando a /login");
-    res.redirect("/login");
-});
+usersViewRouter.get("/fail-login", usersViewController.failLogin);
 
 usersViewRouter.get("/auth/google",passport.authenticate('google',{scope:["email", "profile"]}))
 
@@ -74,22 +29,9 @@ usersViewRouter.get("/auth/google/callback",
     })
   );
 
-usersViewRouter.post("/logout", (req, res, next) => {
-    req.logout((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/login");
-    });
-});
+usersViewRouter.post("/logout", usersViewController.logout);
 
-usersViewRouter.get("/", async (req, res) => {
-    try {
-        res.redirect('/login');
-    } catch (error) {
-        res.status(error.code || 500).send(`<h1>Error</h1><h3>${error.message}</h3>`);
-    }
-});
+usersViewRouter.get("/", usersViewController.rootRedirect);
 
 
 export default usersViewRouter;
